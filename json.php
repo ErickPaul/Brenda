10395727e403f3968b39c5058c14b5cb0510dacb
ErickPaul <ericpaul.karanja@gmail.com>
2016-11-23 15:24:38 -0800

    Update json.php

--- json.php
+++ json.php
@@ -1,208 +1,75 @@
-[{
-	"name": "1",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%80"
-},
 {
-	"name": "2",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C"
-},
-{
-	"name": "3",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%89"
-},
-{
-	"name": "4",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%9B%9B"
-},
-{
-	"name": "5",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%94"
-},
-{
-	"name": "6",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AD"
-},
-{
-	"name": "7",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%83"
-},
-{
-	"name": "8",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AB"
-},
-{
-	"name": "9",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B9%9D"
-},
-{
-	"name": "10",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81"
-},
-{
-	"name": "11",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "12",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%BA%8C"
-},
-{
-	"name": "13",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%B8%89"
-},
-{
-	"name": "14",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E5%9B%9B"
-},
-{
-	"name": "15",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%BA%94"
-},
-{
-	"name": "16",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E5%85%AD"
-},
-{
-	"name": "17",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%B8%83"
-},
-{
-	"name": "18",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E5%85%AB"
-},
-{
-	"name": "19",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%B9%9D"
-},
-{
-	"name": "20",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E5%8D%81"
-},
-{
-	"name": "21",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "22",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E5%8D%81%E4%BA%8C"
-},
-{
-	"name": "23",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E5%8D%81%E4%B8%89"
-},
-{
-	"name": "30",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%89%E5%8D%81"
-},
-{
-	"name": "31",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%89%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "32",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%89%E5%8D%81%E4%BA%8C"
-},
-{
-	"name": "40",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%9B%9B%E5%8D%81"
-},
-{
-	"name": "41",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%9B%9B%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "42",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%9B%9B%E5%8D%81%E4%BA%8C"
-},
-{
-	"name": "50",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%94%E5%8D%81"
-},
-{
-	"name": "51",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%94%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "52",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%94%E5%8D%81%E4%BA%8C"
-},
-{
-	"name": "60",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AD%E5%8D%81"
-},
-{
-	"name": "61",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AD%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "70",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%83%E5%8D%81"
-},
-{
-	"name": "71",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%83%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "80",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AB%E5%8D%81"
-},
-{
-	"name": "81",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%85%AB%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "90",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B9%9D%E5%8D%81"
-},
-{
-	"name": "91",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B9%9D%E5%8D%81%E4%B8%80"
-},
-{
-	"name": "100",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E7%99%BE"
-},
-{
-	"name": "150",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E7%99%BE%E4%BA%94%E5%8D%81"
-},
-{
-	"name": "200",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E7%99%BE"
-},
-{
-	"name": "300",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%89%E7%99%BE"
-},
-{
-	"name": "1000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%83"
-},
-{
-	"name": "1500",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%83%E4%BA%94%E7%99%BE"
-},
-{
-	"name": "2000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%BA%8C%E5%8D%83"
-},
-{
-	"name": "10,000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%80%E4%B8%87"
-},
-{
-	"name": "100,000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%81%E4%B8%87"
-},
-{
-	"name": "1,000,000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E7%99%BE%E4%B8%87"
-},
-{
-	"name": "10,000,000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E5%8D%83%E4%B8%87"
-},
-{
-	"name": "100,000,000",
-	"image": "http:\/\/inu.tapptic.com\/test\/image.php?text=%E4%B8%80%E5%84%84"
-}]
+"title":"About Canada",
+"rows":[
+	{
+	"title":"Beavers",
+	"description":"Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony",
+	"imageHref":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
+	},
+	{
+	"title":"Flag",
+	"description":null,
+	"imageHref":"http://images.findicons.com/files/icons/662/world_flag/128/flag_of_canada.png"
+	},
+	{
+	"title":"Transportation",
+	"description":"It is a well known fact that polar bears are the main mode of transportation in Canada. They consume far less gas and have the added benefit of being difficult to steal.",
+	"imageHref":"http://1.bp.blogspot.com/_VZVOmYVm68Q/SMkzZzkGXKI/AAAAAAAAADQ/U89miaCkcyo/s400/the_golden_compass_still.jpg"
+	},
+	{
+	"title":"Hockey Night in Canada",
+	"description":"These Saturday night CBC broadcasts originally aired on radio in 1931. In 1952 they debuted on television and continue to unite (and divide) the nation each week.",
+	"imageHref":"http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg"
+	},
+	{
+	"title":"Eh",
+	"description":"A chiefly Canadian interrogative utterance, usually expressing surprise or doubt or seeking confirmation.",
+	"imageHref":null
+	},
+	{
+	"title":"Housing",
+	"description":"Warmer than you might think.",
+	"imageHref":"http://icons.iconarchive.com/icons/iconshock/alaska/256/Igloo-icon.png"
+	},
+	{
+	"title":"Public Shame",
+	"description":" Sadly it's true.",
+	"imageHref":"http://static.guim.co.uk/sys-images/Music/Pix/site_furniture/2007/04/19/avril_lavigne.jpg"
+	},
+	{
+	"title":null,
+	"description":null,
+	"imageHref":null
+	},
+	{
+	"title":"Space Program",
+	"description":"Canada hopes to soon launch a man to the moon.",
+	"imageHref":"http://files.turbosquid.com/Preview/Content_2009_07_14__10_25_15/trebucheta.jpgdf3f3bf4-935d-40ff-84b2-6ce718a327a9Larger.jpg"
+	},
+	{
+	"title":"Meese",
+	"description":"A moose is a common sight in Canada. Tall and majestic, they represent many of the values which Canadians imagine that they possess. They grow up to 2.7 metres long and can weigh over 700 kg. They swim at 10 km/h. Moose antlers weigh roughly 20 kg. The plural of moose is actually 'meese', despite what most dictionaries, encyclopedias, and experts will tell you.",
+	"imageHref":"http://caroldeckerwildlifeartstudio.net/wp-content/uploads/2011/04/IMG_2418%20majestic%20moose%201%20copy%20(Small)-96x96.jpg"
+	},
+	{
+	"title":"Geography",
+	"description":"It's really big.",
+	"imageHref":null
+	},
+	{
+	"title":"Kittens...",
+	"description":"Éare illegal. Cats are fine.",
+	"imageHref":"http://www.donegalhimalayans.com/images/That%20fish%20was%20this%20big.jpg"
+	},
+	{
+	"title":"Mounties",
+	"description":"They are the law. They are also Canada's foreign espionage service. Subtle.",
+	"imageHref":"http://3.bp.blogspot.com/__mokxbTmuJM/RnWuJ6cE9cI/AAAAAAAAATw/6z3m3w9JDiU/s400/019843_31.jpg"
+	},
+	{
+	"title":"Language",
+	"description":"Nous parlons tous les langues importants.",
+	"imageHref":null
+	}
+]
+}
